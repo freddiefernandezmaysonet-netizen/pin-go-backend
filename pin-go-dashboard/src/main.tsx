b975d61e2a42013e945cab795b7ab011a0c42c31
Freddie <pe-nf@hotmail.com>
2026-03-10 08:22:34 -0400

    connect AuthProvider

--- pin-go-dashboard/src/main.tsx
+++ pin-go-dashboard/src/main.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { router } from "./app/routes/router";
+import { AuthProvider } from "./auth/AuthProvider";
+
 import "./index.css";
 
 const qc = new QueryClient();
@@ -10,7 +13,9 @@ const qc = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={qc}>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
