80a0afe8f89150368d4c6f6132c37211ade7aedc
Freddie <pe-nf@hotmail.com>
2026-06-22 17:12:56 -0400

    Add market season catalog file

---
+++ src/data/market-season-catalog.ts
@@ -0,0 +1,91 @@
+export type MarketSeasonCatalogItem = {
+  country: string;
+  region: string | null;
+  seasons: {
+    name: string;
+    startMonth: number;
+    startDay: number;
+    endMonth: number;
+    endDay: number;
+    adjustmentPercent: number;
+  }[];
+};
+
+export const MARKET_SEASON_CATALOG: MarketSeasonCatalogItem[] = [
+  {
+    country: "Puerto Rico",
+    region: null,
+    seasons: [
+      {
+        name: "Holiday Peak",
+        startMonth: 12,
+        startDay: 15,
+        endMonth: 4,
+        endDay: 7,
+        adjustmentPercent: 35,
+      },
+      {
+        name: "Spring Break",
+        startMonth: 3,
+        startDay: 1,
+        endMonth: 4,
+        endDay: 15,
+        adjustmentPercent: 20,
+      },
+      {
+        name: "Summer Travel",
+        startMonth: 6,
+        startDay: 1,
+        endMonth: 8,
+        endDay: 31,
+        adjustmentPercent: 15,
+      },
+      {
+        name: "Low Season",
+        startMonth: 8,
+        startDay: 1,
+        endMonth: 11,
+        endDay: 21,
+        adjustmentPercent: -10,
+      },
+    ],
+  },
+  {
+    country: "United States",
+    region: null,
+    seasons: [
+      {
+        name: "Spring Break",
+        startMonth: 3,
+        startDay: 1,
+        endMonth: 4,
+        endDay: 15,
+        adjustmentPercent: 15,
+      },
+      {
+        name: "Summer Peak",
+        startMonth: 6,
+        startDay: 1,
+        endMonth: 8,
+        endDay: 31,
+        adjustmentPercent: 20,
+      },
+      {
+        name: "Thanksgiving",
+        startMonth: 11,
+        startDay: 20,
+        endMonth: 11,
+        endDay: 30,
+        adjustmentPercent: 20,
+      },
+      {
+        name: "Christmas Peak",
+        startMonth: 12,
+        startDay: 15,
+        endMonth: 1,
+        endDay: 7,
+        adjustmentPercent: 35,
+      },
+    ],
+  },
+];
